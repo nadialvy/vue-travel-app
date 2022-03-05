3b4348d2afb6d76d2222035b9e1d5eb9f96a4611
nadialvy <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2022-03-05 20:05:51 +0700

    Navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-  </div>
- 
+<template> 
+  <TheNavigation></TheNavigation>
   <div class="container">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import TheNavigation from '@/components/TheNavigation.vue'
 
+  export default{
+    components: {
+      TheNavigation
+    }
+  }
 </script>
 
 
