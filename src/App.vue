9efc40d6f171fd225c19a742002c4498534d7f17
nadialvy <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2022-03-05 19:51:32 +0700

    vue router

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+ 
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+
