9ce85da56416b300856d03ecaab974402cc1fdc7
nadialvy <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2022-03-05 19:57:21 +0700

    lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import About from '@/views/Tentang.vue'
-import Brazil from '@/views/Brazil.vue'
-import Hawaii from '@/views/Hawaii.vue'
-import Jamaica from '@/views/Jamaica.vue'
-import Panama from '@/views/Panama.vue'
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
-    {path: '/about', name: 'About', component: About},
-    {path: '/brazil', name: 'Brazil', component: Brazil},
-    {path: '/hawaii', name: 'Hawaii', component: Hawaii},
-    {path: '/panama', name: 'Panama', component: Panama},
-    {path: '/jamaica', name: 'Jamaica', component: Jamaica},
+    {path: '/brazil', name: 'Brazil', component: ()=>import('@/views/Brazil.vue')},
+    {path: '/hawaii', name: 'Hawaii', component: ()=>import('@/views/Hawaii.vue')},
+    {path: '/panama', name: 'Panama', component: ()=>import('@/views/Panama.vue')},
+    {path: '/jamaica', name: 'Jamaica', component: ()=>import('@/views/Jamaica.vue')},
 ]
 
 const router = createRouter({
